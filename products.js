8f53a85d9170d61c4d9181e907f2d42947483d7f
LUKO <r224215934@gmail.com>
2026-03-27 09:28:18 +0800

    Update product tags from '相機' to 'FGO'

--- products.js
+++ products.js
@@ -19,7 +19,7 @@ var SAMPLE_PRODUCTS = [
     origin: "日本",
     purchaseDate: "2022-11",
     damage: "輕微使用痕跡",
-    tags: ["相機", "3C"],
+    tags: ["FGO", "3C"],
     dateAdded: "2024-03-10",
     stores: [
       { name: "蝦皮賣場", url: "https://shopee.tw", type: "shopee" },
