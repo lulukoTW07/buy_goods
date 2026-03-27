e75373075a2e180eab7bb0b5d1f1594b67413dd8
lulukoTW07 <r224215934@gmail.com>
2026-03-27 20:07:10 +0000

    Deploying to main from @ lulukoTW07/buy_goods@21e5fc188dd2902500aba6f8426001ae1ba2d1b3 🚀

--- products.js
+++ products.js
@@ -20,17 +20,64 @@
         }
       ]
     },
-
+    {
+      "id": "p001",
+      "name": "Sony ZV-E10 相機（附 16-50mm 鏡頭）",
+      "image": "https://images.unsplash.com/photo-1502982720700-bfff97f2ecac?w=600&q=80",
+      "description": "使用約一年，機況良好，快門次數少，附原廠包裝盒、備用電池兩顆、充電器。",
+      "origin": "日本",
+      "purchaseDate": "2022-11",
+      "damage": "輕微使用痕跡",
+      "tags": [
+        "相機",
+        "3C"
+      ],
+      "dateAdded": "2024-03-10",
+      "stores": [
+        {
+          "name": "蝦皮賣場",
+          "url": "https://shopee.tw",
+          "type": "shopee"
+        },
+        {
+          "name": "賣貨便",
+          "url": "https://buy.line.me",
+          "type": "convenient"
+        }
+      ]
+    },
+    {
+      "id": "p002",
+      "name": "村上春樹《挪威的森林》精裝版",
+      "image": "https://images.unsplash.com/photo-1512820790803-83ca734da794?w=600&q=80",
+      "description": "全新未讀，書封完整，無劃線。2020年出版精裝版。",
+      "origin": "台灣",
+      "purchaseDate": "2023-05",
+      "damage": "全新未拆",
+      "tags": [
+        "書籍"
+      ],
+      "dateAdded": "2024-03-12",
+      "stores": [
+        {
+          "name": "蝦皮賣場",
+          "url": "https://shopee.tw",
+          "type": "shopee"
+        }
+      ]
+    },
     {
       "id": "p003",
       "name": "【全新】旅人筆記本（橄欖）$1000",
       "image": "https://shoplineimg.com/5f2a6b5d2c1b620020264616/6443c30f89b2780001acdd1d/800x.jpg",
       "description": "去年在成田機場多買的",
       "origin": "日本",
-      "purchaseDate": "2028-08",
+      "purchaseDate": "2026-08",
       "damage": "近全新",
-      "tags": ["手帳","全新"],
-      "dateAdded": "2025-08",
+      "tags": [
+        "手帳"
+      ],
+      "dateAdded": "2026-08",
       "stores": [
         {
           "name": "賣貨便",
