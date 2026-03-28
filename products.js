2b36de61c63d4e634df26b4841b10ad62420750b
lulukoTW07 <r224215934@gmail.com>
2026-03-28 19:45:41 +0000

    Deploying to main from @ lulukoTW07/buy_goods@7741aecf2ed2875fe33dd1a153058e5f3f539625 🚀

--- products.js
+++ products.js
@@ -6,13 +6,13 @@
 
 // 可用標籤（可在管理模式中新增/刪除）
 var DEFAULT_TAGS = [
-  "FGO", "二手", "全新", "手帳", "紙膠帶和貼紙","聯絡賣家plurk@fly811"
+  "FGO", "二手", "全新", "手帳", "紙膠帶和貼紙","週邊","聯絡賣家plurk@fly811",
 ];
 
 // 商品資料範例
 var SAMPLE_PRODUCTS = [
     {
-      "id": "p1774641113520",
+      "id": "p100",
       "name": "赤井秀一徽章 $30",
       "image": "https://images.plurk.com/7KUpmSRhHZinqvUsBnG7Ul.jpg",
       "description": "幾年前在藏壽司轉到的。\n商品很小，會用郵寄平信的給買家，運費約8-10元，總價會是30-40元，有需要請用噗浪聯絡我。",
@@ -20,7 +20,7 @@ var SAMPLE_PRODUCTS = [
       "purchaseDate": "忘了（？）",
       "damage": "近全新",
       "tags": [
-        "二手"
+        "二手","週邊",
       ],
       "dateAdded": "2026-03-27",
       "stores": [
@@ -35,15 +35,15 @@ var SAMPLE_PRODUCTS = [
       "id": "p001",
       "name": "聯絡賣家",
       "image": "https://images.plurk.com/caJaFRMFks5zfSx9FkICA.png",
-      "description": "⭐所有在標題上的商品價格皆不含運。\n⭐請點選下方的其他賣場連進噗浪，並用私噗聯繫我，感謝！\n⭐此賣場只為統合用，若需要購買證明須以第三方平台為主唷～",
+      "description": "⭐所有在標題上的商品價格皆不含運。\n⭐請點選下方的按鈕連進噗浪，並用私噗聯繫我，感謝！\n⭐此賣場只為統合用，若需要購買證明須以第三方平台為主唷～",
       "origin": "臺灣",
       "purchaseDate": "-",
       "damage": "輕微使用痕跡",
-      "tags": [],
+      "tags": ["聯絡賣家plurk@fly811"],
       "dateAdded": "2024-03-10",
       "stores": [
         {
-          "name": "其他賣場",
+          "name": "噗浪",
           "url": "https://www.plurk.com/fly811",
           "type": "default"
         }
@@ -51,21 +51,21 @@ var SAMPLE_PRODUCTS = [
     },
     {
       "id": "p002",
-      "name": "村上春樹",
-      "image": "https://images.unsplash.com/photo-1512820790803-83ca734da794?w=600&q=80",
-      "description": "全新未讀，書封完整，無劃線。2020年出版精裝版。",
-      "origin": "台灣",
-      "purchaseDate": "2023-05",
-      "damage": "全新未拆",
+      "name": "明日方舟-朝隴山山兔毛絨玩偶-凱爾希",
+      "image": "https://images.plurk.com/49k0cVqBlgQWhzAUcLJiKp.jpg",
+      "description": "拆過一次，後來就放在袋子裡沒拿出來過了。",
+      "origin": "中國",
+      "purchaseDate": "2019年",
+      "damage": "二手",
       "tags": [
-        "書籍"
+        "二手","週邊",
       ],
       "dateAdded": "2024-03-12",
       "stores": [
         {
-          "name": "蝦皮賣場",
+          "name": "賣貨便",
           "url": "https://shopee.tw",
-          "type": "shopee"
+          "type": "convenient"
         }
       ]
     },
