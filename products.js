764e44db02aaabb77d6f53f2098d900c6b5921b2
LUKO <r224215934@gmail.com>
2026-03-30 05:48:34 +0800

    Update products.js

--- products.js
+++ products.js
@@ -6,11 +6,119 @@
 
 // 可用標籤（可在管理模式中新增/刪除）
 var DEFAULT_TAGS = [
-  "FGO", "二手", "全新", "手帳", "紙膠帶和貼紙","週邊","聯絡賣家plurk@fly811",
+  "FGO","驅魔","二手","全新", "手帳","紙膠帶和貼紙","週邊","聯絡賣家plurk@fly811",
 ];
 
 // 商品資料範例
 var SAMPLE_PRODUCTS = [
+    {
+      "id": "p1774820497393",
+      "name": "FGO絕對魔獸戰線轉蛋橡膠吊飾 $100",
+      "image": "https://myship.7-11.com.tw/i/cgdm/GM2506300653196/2603301100602011.jpg",
+      "description": "伊絲塔跟安娜因為一直放在蛋殼裡因此凹損嚴重故不賣，有需要請私訊。",
+      "origin": "",
+      "purchaseDate": "2019-12",
+      "damage": "輕微使用痕跡",
+      "tags": [
+        "FGO",
+        "週邊"
+      ],
+      "dateAdded": "2026-03-29",
+      "stores": [
+        {
+          "name": "賣貨便",
+          "url": "https://myship.7-11.com.tw/general/detail/GM2506300653196",
+          "type": "convenient"
+        }
+      ]
+    },
+    {
+      "id": "p1774820367629",
+      "name": "FGO2017年全特異點一番賞 $50、$62",
+      "image": "https://myship.7-11.com.tw/i/cgdm/GM2506300653196/2603301100600967.jpg",
+      "description": "外盒默認有些微灰塵與痕跡，但沒有拆過，如果拆開發現內部有異常可向我進行退貨！\n注意：除了術閃價格為62元外，其餘皆為50元。",
+      "origin": "",
+      "purchaseDate": "",
+      "damage": "近全新",
+      "tags": [
+        "FGO",
+        "週邊"
+      ],
+      "dateAdded": "2026-03-29",
+      "stores": [
+        {
+         "name": "賣貨便",
+          "url": "https://myship.7-11.com.tw/general/detail/GM2506300653196",
+          "type": "convenient"
+        }
+      ]
+    },
+    {
+      "id": "p1774820282874",
+      "name": "2024京mafuSNS風格吊飾 $200/個",
+      "image": "https://myship.7-11.com.tw/i/cgdm/GM2506300653196/2603301100603172.jpg",
+      "description": "保存良好，但因為開過所以皆默認有些微損傷。",
+      "origin": "",
+      "purchaseDate": "2024-12",
+      "damage": "近全新",
+      "tags": [
+        "FGO",
+        "週邊"
+      ],
+      "dateAdded": "2026-03-29",
+      "stores": [
+        {
+
+         "name": "賣貨便",
+          "url": "https://myship.7-11.com.tw/general/detail/GM2506300653196",
+          "type": "convenient"
+        }
+
+      ]
+    },
+    {
+      "id": "p1774820107614",
+      "name": "FGO和三麗鷗合作第二彈軟膠吊飾only狂狗 $100",
+      "image": "https://myship.7-11.com.tw/i/cgdm/GM2506300653196/2603301100604024.jpg",
+      "description": "臉部有黑點污漬，外包裝有些微損傷。",
+      "origin": "",
+      "purchaseDate": "忘了（？）",
+      "damage": "有明顯損傷",
+      "tags": [
+        "FGO",
+        "週邊"
+      ],
+      "dateAdded": "2026-03-29",
+      "stores": [
+        {
+         "name": "賣貨便",
+          "url": "https://myship.7-11.com.tw/general/detail/GM2506300653196",
+          "type": "convenient"
+        }
+
+      ]
+    },
+    {
+      "id": "p1774819994462",
+      "name": "驅魔少年JS眼睛鑰匙圈only林克 $100",
+      "image": "https://myship.7-11.com.tw/i/cgdm/GM2506300653196/2603301100605386.jpg",
+      "description": "26年3月初買的，只拆過一次。",
+      "origin": "",
+      "purchaseDate": "2026-03",
+      "damage": "近全新",
+      "tags": [
+        "驅魔",
+        "週邊"
+      ],
+      "dateAdded": "2026-03-29",
+      "stores": [
+        {
+          "name": "賣貨便",
+          "url": "https://myship.7-11.com.tw/general/detail/GM2506300653196",
+          "type": "convenient"
+        }
+      ]
+    },
     {
       "id": "p100",
       "name": "赤井秀一徽章 $30",
@@ -20,7 +128,7 @@ var SAMPLE_PRODUCTS = [
       "purchaseDate": "忘了（？）",
       "damage": "近全新",
       "tags": [
-        "二手","週邊",
+        "週邊"
       ],
       "dateAdded": "2026-03-27",
       "stores": [
@@ -39,7 +147,9 @@ var SAMPLE_PRODUCTS = [
       "origin": "臺灣",
       "purchaseDate": "-",
       "damage": "輕微使用痕跡",
-      "tags": ["聯絡賣家plurk@fly811"],
+      "tags": [
+        "聯絡賣家plurk@fly811"
+      ],
       "dateAdded": "1900-01-01",
       "stores": [
         {
@@ -51,27 +161,27 @@ var SAMPLE_PRODUCTS = [
     },
     {
       "id": "p002",
-      "name": "明日方舟-朝隴山山兔毛絨玩偶-凱爾希 $200",
+      "name": "明日方舟-朝隴山山兔毛絨玩偶-凱爾希 $300",
       "image": "https://images.plurk.com/49k0cVqBlgQWhzAUcLJiKp.jpg",
-      "description": "拆過一次，後來就放在袋子裡沒拿出來過了。",
+      "description": "拆過一次，後來就放在袋子裡沒拿出來過了。\n賣貨便有詳細說明。",
       "origin": "中國",
       "purchaseDate": "2019年",
-      "damage": "二手",
+      "damage": "有明顯損傷",
       "tags": [
-        "二手","週邊",
+        "週邊"
       ],
       "dateAdded": "2026-3-29",
       "stores": [
         {
           "name": "賣貨便",
-          "url": "https://shopee.tw",
+          "url": "https://myship.7-11.com.tw/general/detail/GM2506300653196",
           "type": "convenient"
         }
       ]
     },
     {
       "id": "p003",
-      "name": "【全新】旅人筆記本（橄欖）$1000",
+      "name": "旅人筆記本（橄欖）$1000",
       "image": "https://images.plurk.com/39xzyFDj85hRQCCUyeZdeW.jpg",
       "description": "去年在成田機場多買的，賣貨便運費會加35請注意～",
       "origin": "日本",
@@ -89,4 +199,4 @@ var SAMPLE_PRODUCTS = [
         }
       ]
     }
-];
+  ],
